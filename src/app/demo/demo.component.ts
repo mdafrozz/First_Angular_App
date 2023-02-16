01c479f2505d2aa90195db5fe5170b5c89e840dd
mdafrozz <106298595+mdafrozz@users.noreply.github.com>
2023-02-16 10:20:09 +0530

    Add: launch BridgeLabz Site in a new Tab on clicking BridgeLabz Logo

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -7,9 +7,15 @@ import { Component } from '@angular/core';
 })
 export class DemoComponent {
   title='HelloWorld';
-  imgUrl = "https://www.educationworld.in/wp-content/uploads/2020/06/BridgeLabz-2.jpg";
+  imgUrl = "https://www.educationworld.in/wp-content/uploads/2020/06/BridgeLabz-2.jpg"; 
 
   ngOnInit(): void{
     this.title = "Hello from Bridgelabz.";
   }
+
+  onClick(event: any){
+    console.log(event);
+    window.open("https://www.bridgelabz.com/");
+  }
 }
+
