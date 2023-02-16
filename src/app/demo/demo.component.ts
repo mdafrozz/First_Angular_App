53361952f58060addc696eda302422e2c83f0063
mdafrozz <106298595+mdafrozz@users.noreply.github.com>
2023-02-16 10:08:30 +0530

    Add: display BridgeLabz Logo using Property Binding Technique

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class DemoComponent {
   title='HelloWorld';
+  imgUrl = "https://www.educationworld.in/wp-content/uploads/2020/06/BridgeLabz-2.jpg";
 
   ngOnInit(): void{
     this.title = "Hello from Bridgelabz.";
